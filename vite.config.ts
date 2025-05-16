f688746f57d0744463531cd2841955b9ff0eeb4b
roccosalata <roccosalata@gmail.com>
2025-05-16 20:27:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// vite.config.ts
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/madridinsider/', // must match your repo name
+  // ...other config options
+})
